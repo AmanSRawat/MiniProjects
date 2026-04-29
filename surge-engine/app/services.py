c6d33441f7c4a9e821e60c495734d32fd88172b2
AmanSRawat <amamrawat96@gmail.com>
2026-04-29 16:48:06 +0530

    integrated the weather api logic

---
+++ surge-engine/app/services.py
@@ -0,0 +1,38 @@
+import os
+import requests
+import datetime as dt
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("api_key")
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+def fetch_weather_data(city: str):
+    params = {
+        "q": city,
+        "appid": API_KEY,
+        "units": "metric"
+    }
+    
+    try:
+        response = requests.get(BASE_URL, params=params)
+        
+        response.raise_for_status() 
+        
+        data = response.json()
+        return {
+            "city": data["name"],
+            "temperature": data["main"]["temp"],
+            "condition": data["weather"][0]["main"], 
+            "description": data["weather"][0]["description"],
+            "timestamp": dt.datetime.fromtimestamp(data["dt"])
+        }
+        
+    except requests.exceptions.HTTPError as err:
+        return {"error": f"HTTP Error: {err.response.status_code}", "msg": err.response.text}
+    except Exception as e:
+        return {"error": str(e)}
+
+weather = fetch_weather_data("Dehradun")
+print(weather)
